633cc232cbad234593633b0dc965a0699d35a3da
mm20180054 <milos.matke999@gmail.com>
2024-07-09 17:46:37 +0200

    Omoguceno kreiranje publikacije pomocu novokreirane reusable komponente PublicationInfo

--- frontend/naucnoIstrazivackaAgencijaAngular/src/app/components/add-publication/add-publication.component.ts
+++ frontend/naucnoIstrazivackaAgencijaAngular/src/app/components/add-publication/add-publication.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { PublicationInfoComponent } from '../publication-info/publication-info.component';
+import { Publication } from '../../domain/publication.domain';
+import { PublicationsService } from '../../service/publications.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-add-publication',
   standalone: true,
-  imports: [],
-  templateUrl: './add-publication.component.html',
-  styleUrl: './add-publication.component.scss'
+  imports: [PublicationInfoComponent],
+  template: `
+    <app-publication-info
+      (publicationEmitter)="savePublication($event)"
+    ></app-publication-info>
+  `,
+  styleUrl: './add-publication.component.scss',
 })
 export class AddPublicationComponent {
+  constructor(
+    public publicationsService: PublicationsService,
+    public location: Location
+  ) {}
 
+  savePublication($event: Publication) {
+    this.publicationsService
+      .savePublication($event)
+      .pipe()
+      .subscribe((x) => {
+        console.log('============ID:=============');
+        console.log(x.id);
+        this.location.back();
+      });
+  }
 }
